ed96f26afbe2a0c1fbf7889e91f41d4101c59435
Simon <simjenner3@gmail.com>
2017-09-09 23:07:27 +0100

    Actually push files lol

---
+++ source/maingui.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'maingui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.editor = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.editor.setGeometry(QtCore.QRect(0, 0, 801, 561))
+        self.editor.setTabStopWidth(40)
+        self.editor.setObjectName("editor")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionLoad_py_or_hex = QtWidgets.QAction(MainWindow)
+        self.actionLoad_py_or_hex.setObjectName("actionLoad_py_or_hex")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSave_As = QtWidgets.QAction(MainWindow)
+        self.actionSave_As.setObjectName("actionSave_As")
+        self.actionCompile_To_hex = QtWidgets.QAction(MainWindow)
+        self.actionCompile_To_hex.setObjectName("actionCompile_To_hex")
+        self.actionReadme = QtWidgets.QAction(MainWindow)
+        self.actionReadme.setObjectName("actionReadme")
+        self.menuFile.addAction(self.actionLoad_py_or_hex)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSave_As)
+        self.menuFile.addAction(self.actionCompile_To_hex)
+        self.menuHelp.addAction(self.actionReadme)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Micro:bit Python3 IDE"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionLoad_py_or_hex.setText(_translate("MainWindow", "Load (.py or .hex)"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionSave_As.setText(_translate("MainWindow", "Save As..."))
+        self.actionCompile_To_hex.setText(_translate("MainWindow", "Compile To .hex"))
+        self.actionReadme.setText(_translate("MainWindow", "Readme"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ source/syntax.py
@@ -0,0 +1,132 @@
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QSyntaxHighlighter, QColor, QTextCharFormat, QFont
+
+boolean = ["False", "None", "True"]
+
+keywords = [
+    "and", "as", "assert", "break", "class", "continue", "def", "del", "elif",
+    "else", "except", "finally", "for", "from", "global", "if", "import", "in",
+    "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try",
+    "while", "with", "yield", "self"
+]
+
+comparators_arithmetic_bitwise = [
+    "<", ">", "!", "+", "-", "*", "/", "%", "=", "|", "^", "&", "~"
+]
+
+class PythonSyntaxHighlighter(QSyntaxHighlighter):
+    def __init__(self, document):
+        QSyntaxHighlighter.__init__(self, document)
+
+        self.highlighting_rules = []
+        """
+        pattern = regex pattern, format = colour/font formatting
+        all colour names http://www.december.com/html/spec/colorsvg.html
+        """
+
+        # Keywords
+        k_format = QTextCharFormat()
+        k_colour = QColor()
+        k_colour.setRgb(153, 137, 234)
+        k_format.setForeground(k_colour)
+        k_format.setFontWeight(QFont.Bold)
+        for word in keywords:
+            pattern = QRegExp("\\b" + word + "\\b")
+            self.highlighting_rules.append({"pattern":pattern, "format":k_format})
+
+        # Boolean
+        b_format = QTextCharFormat()
+        b_colour = QColor()
+        b_colour.setRgb(234, 61, 61)
+        b_format.setForeground(b_colour)
+        for word in boolean:
+            pattern = QRegExp("\\b" + word + "\\b")
+            self.highlighting_rules.append({"pattern":pattern, "format":b_format})
+
+        # Comparators, arithmetic & bitwise
+        cab_format = QTextCharFormat()
+        cab_colour = QColor()
+        cab_colour.setRgb(199, 146, 234)
+        cab_format.setForeground(cab_colour)
+        for symbol in comparators_arithmetic_bitwise:
+            pattern = QRegExp("\\"+symbol)
+            self.highlighting_rules.append({"pattern":pattern, "format":cab_format})
+
+        num_format = QTextCharFormat()
+        num_colour = QColor()
+        num_colour.setNamedColor("darkorange")
+        num_format.setForeground(num_colour)
+        num_pattern = QRegExp("[0-9]")
+        hex_pattern = QRegExp("0[xX][0-9a-fA-F]+")
+
+        self.string_format = QTextCharFormat()
+        s_colour = QColor()
+        s_colour.setRgb(124, 179, 41)
+        self.string_format.setForeground(s_colour)
+
+        s_pattern1 = QRegExp( '\".*\"' )
+        s_pattern1.setMinimal(True)  # Don't be greedy :(
+
+        s_pattern2 = QRegExp( "\'(.*)\'" )
+        s_pattern2.setMinimal(True)
+
+        comment_format = QTextCharFormat()
+        comment_colour = QColor()
+        comment_colour.setNamedColor("grey")
+        comment_format.setForeground(comment_colour)
+        comment_pattern = QRegExp("\#.*")
+
+        """
+        Add to dict in the same order they should be applied (For example,
+        string highlighting needs to be applied after symbols, so if there is
+        a symbol in a string, it will be part of the string instead of having
+        the symbol highlighting)
+        """
+        self.highlighting_rules.append({"pattern":num_pattern, "format":num_format})
+        self.highlighting_rules.append({"pattern":hex_pattern, "format":num_format})
+        self.highlighting_rules.append({"pattern":comment_pattern, "format":comment_format})
+        self.highlighting_rules.append({"pattern":s_pattern1, "format":self.string_format})
+        self.highlighting_rules.append({"pattern":s_pattern2, "format":self.string_format})
+
+    def highlightBlock(self, text):
+        for rule_dict in self.highlighting_rules:
+            expression = QRegExp(rule_dict["pattern"])
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, rule_dict["format"])
+                index = expression.indexIn(text, index + length)
+        self.setCurrentBlockState(0)
+
+        self.match_multiline_comment(text, QRegExp('"""'), 1)
+        self.match_multiline_comment(text, QRegExp("'''"), 2)
+
+    def match_multiline_comment(self, text, delimiter, block_state):
+        """
+        This does not match multiline comments 100% but its good enough
+        """
+        # If inside multiline comment, start at index 0
+        if self.previousBlockState() == block_state:
+            start = 0
+            offset = 0
+        else:
+            start = delimiter.indexIn(text)
+            offset = 3  # Len of """
+
+        # If there is a match
+        if start >= 0:
+            # Look for the ending delimiter
+            end = delimiter.indexIn(text, start + offset)
+
+            # If found
+            if end >= start + offset:
+                # Format up to and including delimiter
+                length = end + 3
+                self.setCurrentBlockState(0)
+            else:
+                # Format whole line and let next call know it is still in block
+                self.setCurrentBlockState(block_state)
+                length = len(text)
+
+            # Format text
+            self.setFormat(start, length, self.string_format)
