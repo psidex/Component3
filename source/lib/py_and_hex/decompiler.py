72880d139094b9e5a5479b882558ae53545dba12
Simon <simjenner3@gmail.com>
2017-09-11 19:47:01 +0100

    Lib

---
+++ source/lib/py_and_hex/decompiler.py
@@ -0,0 +1,36 @@
+def decompile(file_path):
+    """
+    Returns -1 if failed, otherwise, return script text
+    """
+    output = ""
+    first = True
+
+    with open(file_path, "rb") as f:
+        # Skips firmware.hex
+        data = f.readlines()
+        if len(data) < 15158:
+            return -1  # Too short, cannot be MicroPython file
+        data = data[15155:-2]
+
+    for record in data:
+        if first:
+            # Check for MP headers then ignore MP headers
+            if record[9:13] != b"4D50":
+                return -1
+            data = record[17:-2]
+            first = False
+        else:
+            data = record[9:-2]
+        chunk = []
+        last = 0
+        for x in range(2, len(data), 2):
+            chunk.append(data[last:x])
+            last = x
+        for y in chunk:
+            c = chr(int(y.decode("utf-8"), 16))
+            if c != None:
+                output += c
+    return output
+
+if __name__ == "__main__":
+    print(de_compile("microbit.hex"))
