ebe22d5884bc297341f38b1d5732bd99de40f0e3
Simon <simjenner3@gmail.com>
2017-11-03 15:33:55 +0000

    Documented tab handler a bit better

--- lib/qt/lib/tab_handler.py
+++ lib/qt/lib/tab_handler.py
@@ -1,21 +1,47 @@
-from PyQt5.QtWidgets import QWidget
-
 """
-tab_handler
-Controls a Qt tab widget. Does not allow closing of last tab.
+class - tab_handler
+___________________
+A basic handler for PyQt5's QTabWidget to be paired with a QPlainTextEdit
+
+When a tab is clicked on the QTabWidget, the QPlainTextEdit will be
+automatically switched to that tabs saved text. There is no maximum number of
+tabs, and the widget can never have less than 1 tab
+
+Arg num + name     : Type           : Default  : Description
+--------------------------------------------------------------
+1 tab_widget       : QTabWidget     : -        : The QTabWidget to use
+2 text_edit_widget : QPlainTextEdit : -        : The QPlainTextEdit to use
+3 default_name     : String         : "new.py" : The default name for any new tabs
 
 Example usage:
-my_tab_handler = tab_handler(editor_tab_widget, editor_text_edit)
-editor_tab_widget.tabCloseRequested.connect(my_tab_handler.tab_closed)
-editor_tab_widget.tabBarClicked.connect(my_tab_handler.change_tab)
-editor_tab_widget.addTab(QtWidgets.QWidget(), "new.py")
-create_new_file_btn.clicked.connect(lambda: my_tab_handler.load_file("new.py", ""))
+my_tab_handler = tab_handler(my_tab_widget, my_plain_text_edit)
+
+
+function - tab_handler.new_tab
+______________________________
+Creates a new tab on the tab widget and moves to that tab
+
+Arg num + name : Type   : Default           : Description
+--------------------------------------------------------------
+1 editor_text  : String : ""                : The text to be inserted into the text edit
+2 tab_name     : String : self.default_name : The name of the new tab
+
+Example usage:
+new_file_button.clicked.connect(my_tab_handler.new_tab)
 """
+
+from PyQt5.QtWidgets import QWidget
+
 class tab_handler(object):
-    def __init__(self, tab_widget, text_edit_widget):
+    def __init__(self, tab_widget, text_edit_widget, default_name="new.py"):
         self.tab_widget = tab_widget
         self.text_edit_widget = text_edit_widget
-        self.tab_texts = [""]
+        self.tab_texts = []
+        self.default_tab_name = default_name
+        # Auto bind stuff
+        text_edit_widget.tabCloseRequested.connect(self.tab_closed)
+        text_edit_widget.tabBarClicked.connect(self.change_tab)
+        self.new_tab()
 
     def tab_closed(self, index):
         if self.tab_widget.count() == 1:
@@ -38,7 +64,11 @@ class tab_handler(object):
         self.tab_texts[self.tab_widget.currentIndex()] = self.text_edit_widget.toPlainText()
         self.text_edit_widget.setPlainText(self.tab_texts[index])
 
-    def new_tab(self, tab_name, editor_text):
+    def new_tab(self, editor_text=None, tab_name=None):
+        if not editor_text:
+            editor_text = ""
+        if not tab_name:
+            tab_name = self.default_tab_name
         self.tab_widget.addTab(QWidget(), tab_name)
         self.tab_texts.append(editor_text)
         self.tab_texts[self.tab_widget.currentIndex()] = self.text_edit_widget.toPlainText()
