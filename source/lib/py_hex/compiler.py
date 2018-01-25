00779a61aed698a29c9a41861311afbf6f9a9e5c
Simon <thatguywiththatname@users.noreply.github.com>
2018-01-25 09:56:03 +0000

    Fixed

--- source/lib/py_hex/compiler.py
+++ source/lib/py_hex/compiler.py
@@ -1,19 +1,3 @@
-"""
-TO FIX
-Compiling is missing the last bracket and \n in this script:
-```
-from microbit import *
-
-while True:
-    display.scroll("1234")
-    sleep(500)
-
-```
-Mightbe because the script needs to be padded
-with 0's before it is iterated over to produce
-the .hex
-"""
-
 def compile_to_hex(script_bin):
     script_size = len(script_bin)
     if script_size > 0x2000:
@@ -25,20 +9,20 @@ def compile_to_hex(script_bin):
 
     # Prepend header + low + high bytes of script length
     script_bin = b"\x4D\x50" + bytes([script_size & 0xFF]) + bytes([(script_size >> 8) & 0xFF]) + script_bin
-
-    for i in range(0, script_size, 16):
+    # Pad with trailing 0x00s (so len is a multiple of 16)
+    script_bin += b"\x00" * (16 - (len(script_bin) % 16))
+    
+    for i in range(0, len(script_bin), 16):
         record = []
         record.append(0x10)                # Len of data section
         record.append((addr >> 8) & 0xff)  # High byte of address
         record.append(addr & 0xff)         # Low byte
         record.append(0x0)                 # Data type
         for i in range(16):                # 16 chars from script
-            try:
-                record.append(script_bin[offset + i])
-            except IndexError:
-                record.append(0x00)  # Script finished, pad with 0s
-        # Checksum - twos complement of record+, then get low byte
+            record.append(script_bin[offset + i])
+        # Checksum - twos complement of record, then get low byte
         record.append(((sum(record) ^ 0xFF) + 0x1) & 0xFF)
+        # Format record as a string of hex characters
         output.append(":" + "".join("{:02x}".format(x) for x in record).upper())
         addr += 16    # Next address
         offset += 16  # Next script location
